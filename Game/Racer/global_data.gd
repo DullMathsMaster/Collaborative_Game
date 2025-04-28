2d168ecf46ae3f87b7b3c75cf282cc1ddd80c5ff
Wilson <fkkr58@durham.ac.uk>
2025-04-28 12:37:47 +0100

    Broken merge, reverting back

--- Game/Racer/global_data.gd
+++ Game/Racer/global_data.gd
@@ -6,4 +6,9 @@ var number_of_laps = 0
 var player_one_car = -1 # The index of the car in `car_paths`
 var player_two_car = -1 # `-1` means none selected
 
-var car_paths = ["assets/models/", "assets/models/"]
+var car_path_header = "assets/models/OBJ format/" # The directory the models are stored in
+var car_file_types = ".obj" # The file extension for the car models
+var car_paths = ["ambulance", "delivery-flat", "delivery", "firetruck",
+"garbage-truck", "hatchback-sports", "police", "race-future", "race",
+"sedan-sports", "sedan", "suv-luxury", "suv", "taxi", "tractor-police", "tractor-shovel",
+"tractor", "truck-flat", "truck", "van"]
--- Game/Racer/main-menu/racer-selection/racer_selection.gd
+++ Game/Racer/main-menu/racer-selection/racer_selection.gd
@@ -1,8 +1,14 @@
 extends Control
 
+@onready var object_container: HBoxContainer = %ObjectContainer
+@onready var scroll_container: ScrollContainer = %ScrollContainer
 
-var racer_index = 0
-var number_of_racers = len(GlobalData.car_paths)
+
+var target_scroll = 0
+
+
+func _ready() -> void:
+	_set_selection()
 
 
 func _process(delta: float) -> void:
@@ -13,27 +19,70 @@ func _process(delta: float) -> void:
 			UiLoader.load_into_men_space("res://main-menu/racer-selection/racer_selection.tscn")
 		# If player 1 has not selected their car
 		else:
-			UiLoader.load_into_men_space("res://main-menu/player-selection/player_selection.tscn")
+			UiLoader.load_into_men_space("res://main-menu/player-selection/racer_selection.tscn")
 
 
-func _on_confirm_button_pressed() -> void:
-	# If player 1 of 2 has just selected their car
-	if GlobalData.two_player && GlobalData.player_one_car == -1:
-		GlobalData.player_one_car = racer_index
-		UiLoader.load_into_men_space("res://main-menu/racer-selection/racer_selection.tscn")
-	# If player 2 of 2 has just selected their car
-	elif GlobalData.two_player:
-		GlobalData.player_two_car = racer_index
-		UiLoader.load_into_men_space("res://main-menu/track-selection/track_selection.tscn")
-	# If player 1 of 1 has just selected car
-	else:
-		GlobalData.player_one_car = racer_index
-		UiLoader.load_into_men_space("res://main-menu/track-selection/track_selection.tscn")
+func _set_selection():
+	await get_tree().create_timer(0.01).timeout
+	_select_deselect_highlight()
 
 
-func _on_prev_racer_button_pressed() -> void:
-	racer_index = (racer_index - 1) % number_of_racers
+func _on_previous_racer_button_pressed() -> void:
+	var scroll_value = target_scroll - _get_space_between()
+	await _tween_scroll(scroll_value)
+	_select_deselect_highlight()
 
 
 func _on_next_racer_button_pressed() -> void:
-	racer_index = (racer_index + 1) % number_of_racers
+	var scroll_value = target_scroll + _get_space_between()
+	await _tween_scroll(scroll_value)
+	_select_deselect_highlight()
+
+
+func _get_space_between():
+	var distance_size = object_container.get_theme_constant("separation")
+	var object_size = object_container.get_children()[1].size.x
+	
+	return distance_size + object_size
+
+
+func _tween_scroll(scroll_value):
+	target_scroll = scroll_value
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(scroll_container, "scroll_horizontal", scroll_value, 0.25)
+	await tween.finished
+
+
+func _select_deselect_highlight():
+	var selected_node = get_selected_value()[0]
+	
+	for object in object_container.get_children():
+		if object is not TextureRect: continue
+		if object == selected_node: object.modulate = Color(1,1,1)
+		else: object.modulate = Color(0,0,0)
+
+
+func get_selected_value():
+	var selected_position = %"Selection-Marker".global_position
+	var i = -1
+	for object in object_container.get_children():
+		if object.get_global_rect().has_point(selected_position):
+			return [object, i]
+		else: i = i + 1
+
+
+func _on_racer_selection_button_pressed() -> void:
+	var i = get_selected_value()[1]
+	# If player 1 of 2 has just selected their car
+	if GlobalData.two_player && GlobalData.player_one_car == -1:
+		GlobalData.player_one_car = i
+		UiLoader.load_into_men_space("res://main-menu/racer-selection/racer_selection.tscn")
+	# If player 2 of 2 has just selected their car
+	elif GlobalData.two_player:
+		GlobalData.player_two_car = i
+		UiLoader.load_into_men_space("res://main-menu/track-selection/track_selection.tscn")
+	# If player 1 of 1 has just selected car
+	else:
+		GlobalData.player_one_car = i
+		UiLoader.load_into_men_space("res://main-menu/track-selection/track_selection.tscn")
